23a33d45b293649bb2f83b28da844a31918d1ff2
bindu <kamodulab@gmail.com>
2019-07-12 10:23:52 +0530

    Spring Task5 comppleted

--- src/main/java/com/stackroute/domain/BeanLifeCycleDemoBean.java
+++ src/main/java/com/stackroute/domain/BeanLifeCycleDemoBean.java
@@ -1,5 +1,24 @@
 package com.stackroute.domain;
 
-public class BeanLifeCycleDemoBean {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifeCycleDemoBean implements InitializingBean, DisposableBean {
+
+
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initialization method called");
+
+    }
+    public void customInit(){
+        System.out.println("init");
+    }
+    public void customDestroy(){
+        System.out.println("destroy");
+    }
+    public void destroy() throws Exception {
+        System.out.println("destroy method called");
+    }
 
 }
--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -7,23 +7,22 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class Main {
-    public static void main(String[] args)
-    {
-
-
-    ApplicationContext context = new ClassPathXmlApplicationContext("file:src/beans.xml");
-    Movie movie = context.getBean("movie", Movie.class);
-    movie.actorInformation();
+    public static void main(String[] args) throws Exception {
+       AbstractApplicationContext context = new ClassPathXmlApplicationContext("file:src/beans.xml");
+        Movie movie = context.getBean("movie", Movie.class);
+        movie.actorInformation();
 
 
         Movie movie1 = context.getBean("movie1", Movie.class);
         movie1.actorInformation();
 
-
+        BeanLifeCycleDemoBean beanLifeCycleDemoBean = context.getBean("beanLifeCycle", BeanLifeCycleDemoBean.class);
+        context.close();
     }
 }
